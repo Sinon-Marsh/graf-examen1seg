9a32b9e92db3197a085e7f4ffb3d93dd558f1135
Gamaliel Jimenez Oropeza <l23200855@pachuca.tecnm.mx>
2026-09-23 10:14:02 -0600

    Ajustes SVG + Control de dimensión

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// Obtener la imagen y los botones
-const image = document.getElementById('image');
-const smallBtn = document.getElementById('smallBtn');
-const mediumBtn = document.getElementById('mediumBtn');
-const largeBtn = document.getElementById('largeBtn');
-
-// Funciones para cambiar el tamaño de la imagen
-smallBtn.addEventListener('click', () => {
-    image.style.width = '150px';
-    image.style.height = '150px';
-});
-
-mediumBtn.addEventListener('click', () => {
-    image.style.width = '300px';
-    image.style.height = '300px';
-});
-
-largeBtn.addEventListener('click', () => {
-    image.style.width = '450px';
-    image.style.height = '450px';
-});
+function cambiarTamano(ancho) {
+  const img = document.getElementById('imagenLogo');
+  if (img) {
+    img.style.width = ancho;
+  }
+}
